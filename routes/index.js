f9b39c75e1852264812859a78376065aee058ab0
Neeraj <na1993194@gmail.com>
2023-10-14 23:50:53 +0530

    setup the view enginie

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ console.log('router loaded');
 
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// for any further routes, across from here 
+// router.use('/routerName', require('./routerfile));
 
 
 module.exports = router; 
