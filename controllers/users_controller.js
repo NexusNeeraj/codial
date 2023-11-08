af0d71c1817546c4797933f6eb33dee93cea20a0
Neeraj <na1993194@gmail.com>
2023-11-09 02:50:13 +0530

    manual-authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,55 +1,95 @@
 const User = require("../models/user");
 
-module.exports.profile = function(req, res){
-      res.render('users', {
-            title: "User"
-      });
-}
+//const User = require('user-model'); // Replace 'user-model' with the actual path to your User model
+
+module.exports.profile = async function (req, res) {
+  if (req.cookies.user_id) {
+    try {
+      const user = await User.findById(req.cookies.user_id).exec();
+      if (user) {
+        return res.render('user_profile', {
+          title: "User Profile",
+          user: user
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    return res.redirect('/users/sign-in');
+  }
+};
 
 
 // render the sign up page
-module.exports.signUp = function(req, res){
-      res.render('user_sign_up', {
-            title: "Codial | Sign Up"
-      });
-}
-
+module.exports.signUp = function (req, res) {
+  res.render("user_sign_up", {
+    title: "Codial | Sign Up",
+  });
+};
 
 // render the sign in page
-module.exports.signIn = function(req, res){
-      res.render('user_sign_in', {
-            title: "Codial | Sign In"
-      });
-}
+module.exports.signIn = function (req, res) {
+  res.render("user_sign_in", {
+    title: "Codial | Sign In",
+  });
+};
 
 // get the sign up data
-module.exports.create = function(req, res){
-      if(req.body.password != req.body.confirm_password){
-            return res.redirect('back');
-      }
-
-      User.findOne({email: req.body.email}, function(err, user){
-            if(err){
-                  console.log('error in finding user in signing up');
-                  return;
-            }
-
-            if(!user){
-                  User.create(req.body, function(err, user){
-                        if(err){
-                              console.log('Error in creating user while signing up');
-                              return;
-                        }
-                        return res.redirect('/users/sign-in');
-                  })
-            }
-            else{
+module.exports.create = async function (req, res) {
+      try {
+            if (req.body.password != req.body.confirm_password) {
                   return res.redirect('back');
             }
-      });
-}
+
+            const existingUser = await User.findOne({ email: req.body.email });
+
+            if (!existingUser) {
+                  const newUser = await User.create(req.body);
+                  return res.redirect('/users/sign-in');
+            } else {
+                  throw new Error("Email is already in use");
+            }
+      } catch (err) {
+            console.error('Error in signing up:', err);
+            return res.redirect('back');
+      }
+};
+
 
 // sign In and create a session for the user
-module.exports.createSession = function(req, res){
-      // Todo Later
-}
+module.exports.createSession = async function (req, res) {
+      try {
+            // Find the user using await
+            const user = await User.findOne({ email: req.body.email });
+
+            if (!user) {
+                  // Handle user not found
+                  return res.redirect("back");
+            }
+
+            // Handle password mismatch
+            if (user.password !== req.body.password) {
+                  return res.redirect("back");
+            }
+
+            // Handle session creation
+            res.cookie("user_id", user.id);
+            return res.redirect("/users/profile");
+      } catch (err) {
+            console.log("Error in signing in:", err);
+            return res.redirect("back");
+      }
+};
+
+
+// // user sign-out 
+
+module.exports.signOut = function (req, res) {
+      // res.render("user_sign_in", {
+      //   title: "Codial | Sign In",
+      // });
+      res.clearCookie('user_id'); // Replace 'user_id' with the name of your authentication cookie
+      // Redirect to a sign-in page or home page
+      res.redirect('/users/sign-in');
+};
